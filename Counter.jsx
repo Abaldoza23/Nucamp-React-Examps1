6cb5fa40dbab48a7cdeaf16c9b239f7c3336e2fa
Ant Bal <antbal52@gmail.com>
2024-02-28 12:59:46 -0800

    Update Counter

---
+++ Counter.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export function Counter({ name }) {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <div>{name}</div>
+            <button onClick={() => setCount(c => c - 1)}>-</button>
+            {count}
+            <button onClick={() => setCount(c => c + 1)}>+</button>
+        </div>
+    )
+}
+
+function useLocalStorage(key, defaultValue) {
+    
+}
